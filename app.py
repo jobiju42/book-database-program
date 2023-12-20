5c5caaf19705bd7c101b59b748f51d608d8aa329
Joseph LoForti <joeymloforti@outlook.com>
2023-12-19 20:20:13 -0800

    added our database model

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from models  import (Base, session, Book, engine)
+
+
+
+
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to the database
@@ -9,7 +15,8 @@
 
 
 
-
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
 
 
 
